ddda60e889e2e0e1001fb0069867c20302d8d660
Adeos2022 <n49adjoe@gmail.com>
2022-09-19 23:18:44 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,17 @@
-/*
- * File: 3-puts.c
- * Auth: Deogracius Ayile
- */
-
-#include "main.h"
-
-/**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
- */
-void _puts(char *str)
-{
-	while (*str)
-	{
-		_putchar(*str++);
-
-		}
-	_putchar('\n');
+/** 
+ *   * _puts - prints a string in stdout 
+ *    * @str: string to print 
+ *     * 
+ *      * Return: void 
+ *       */ 
+ void _puts(char *str) 
+	 { 
+		          int i = 0; 
+		    
+		           while (*(str + i)) 
+			            { 
+				                     _putchar(*(str + i)); 
+				                      i++; 
+				               } 
+		            _putchar('\n'); 
 }
