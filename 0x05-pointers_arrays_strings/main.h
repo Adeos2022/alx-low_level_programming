71c93b5e77e567e2caee535323e513ed246db62a
Adeos2022 <n49adjoe@gmail.com>
2022-09-20 01:23:39 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN.H
 #define MAIN.H
+#include "main.h" 
+#include <stdio.h>
 int _putchar(char); 
  void reset_to_98(int *n); 
  void swap_int(int *a, int *b); 
@@ -12,3 +14,4 @@ int _putchar(char);
  void print_array(int *a, int n); 
  char *_strcpy(char *dest, char *src); 
  int _atoi(char *s);
+#endif
